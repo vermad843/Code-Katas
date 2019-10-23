4de7859e293b45b4567dfc2df134d32d205235fb
vermad843 <vishnuverma7524@gmail.com>
2019-10-23 22:00:23 +0530

    countString problem

---
+++ epi_29/countString.js
@@ -0,0 +1,24 @@
+function count (string) {  
+    const counts = {};
+
+    for (let i = 0; i < string.length; i++) {
+        const letter = string[i];
+        console.log(letter);
+
+       //if the current letter is not a property in the object 
+       //set it 0 
+       //if current letter is there keep it the same
+      counts[letter] = counts[letter] || 0;
+      counts[letter]++;
+
+   
+    }
+     return counts;
+  }
+
+
+
+
+
+console.log(count("aba"), { a: 2, b: 1 }); 
+console.log(count(""), {});  
