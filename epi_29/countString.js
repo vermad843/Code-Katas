625873a883a8c87d68d6c4b5915b40029d50f52a
vermad843 <vishnuverma7524@gmail.com>
2019-10-25 20:40:19 +0530

    another way to do this

--- epi_29/countString.js
+++ epi_29/countString.js
@@ -1,17 +1,34 @@
-function count (string) {  
+// function count (string) {  
+//     const counts = {};
+
+//     for (let i = 0; i < string.length; i++) {
+//         const letter = string[i];
+//         console.log(letter);
+
+//        //if the current letter is not a property in the object 
+//        //set it 0 
+//        //if current letter is there keep it the same
+//       counts[letter] = counts[letter] || 0;
+//       counts[letter]++;
+
+   
+//     }
+//      return counts;
+//   }
+
+
+  function count (string) {  
     const counts = {};
 
     for (let i = 0; i < string.length; i++) {
         const letter = string[i];
         console.log(letter);
-
-       //if the current letter is not a property in the object 
-       //set it 0 
-       //if current letter is there keep it the same
-      counts[letter] = counts[letter] || 0;
-      counts[letter]++;
-
-   
+                           
+   if(counts.hasOwnProperty(letter)) {
+     counts[letter]++;
+   }else {
+     counts[letter] = 1;
+   }
     }
      return counts;
   }
