b2e3638dfdb0003aa9794d4aa1fe2f2091d60f4a
vermad843 <vishnuverma7524@gmail.com>
2019-10-29 18:06:35 +0530

    alternating words

--- 8kyu.js
+++ 8kyu.js
@@ -60,17 +60,20 @@
 // }
 
 String.prototype.toAlternatingCase = function () {
-    const spliting =  this.split("")
-    console.log(spliting)
-    const value =  spliting.map((spl) => {
-         return  spl === spl.toUpperCase()? spl.toLowerCase(): spl.toUpperCase()
-     })
-      console.log(value)
-         const an =   value.join('')
-           console.log(an);  
-    // .map(a => {
-    //     return a === a.toUpperCase()? a.toLowerCase(): a.toUpperCase()).join('')
-    }  
+    return  this.split("")
+    .map((eachCharacter) => {
+         return  eachCharacter === eachCharacter.toUpperCase()? eachCharacter.toLowerCase(): eachCharacter.toUpperCase()
+     }).join('')
+    }
+    
+    console.log("hello world".toAlternatingCase(), "HELLO WORLD");
+    console.log("HELLO WORLD".toAlternatingCase(), "hello world");
+    console.log("hello WORLD".toAlternatingCase(), "HELLO world");
+    console.log("HeLLo WoRLD".toAlternatingCase(), "hEllO wOrld");
+    console.log("12345".toAlternatingCase(), "12345");
+    console.log("1a2b3c4d5e".toAlternatingCase(), "1A2B3C4D5E");
+    console.log("String.prototype.toAlternatingCase".toAlternatingCase(), "sTRING.PROTOTYPE.TOaLTERNATINGcASE");
+    console.log("Hello World".toAlternatingCase().toAlternatingCase(), "Hello World");
 
 
 
