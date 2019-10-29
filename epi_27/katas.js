387cd152e8aca2ea91850a50801722bce4e1031e
vermad843 <vishnuverma7524@gmail.com>
2019-10-29 15:54:29 +0530

    number to string

--- epi_27/katas.js
+++ epi_27/katas.js
@@ -1,25 +1,37 @@
-function basicOp(operation, value1, value2)
-{
-  const operations = {
-      '+' : (a, b) => a + b,
-      '-' : (a, b) => a - b, 
-      '*' : (a, b) => a * b, 
-      '/' : (a, b) => a / b,
-  };
+// function basicOp(operation, value1, value2)
+// {
+//   const operations = {
+//       '+' : (a, b) => a + b,
+//       '-' : (a, b) => a - b, 
+//       '*' : (a, b) => a * b, 
+//       '/' : (a, b) => a / b,
+//   };
 
-  const operationFn = operations[operation];
+//   const operationFn = operations[operation];
 
-  return operationFn(value1, value2)
+//   return operationFn(value1, value2)
+// }
+
+// // create a object called operations 
+// // define the keys and values there 
+// // from operations use bracket notation to 
+// // access operation 
+// //then return the operationFn
+
+
+// console.log(basicOp('+', 4, 7), 11);
+// console.log(basicOp('-', 15, 18), -3);
+// console.log(basicOp('*', 5, 5), 25);
+// console.log(basicOp('/', 49, 7), 7);
+
+
+function numberToString(num) {
+  return num.toString();
 }
 
-// create a object called operations 
-// define the keys and values there 
-// from operations use bracket notation to 
-// access operation 
-//then return the operationFn
 
 
-console.log(basicOp('+', 4, 7), 11);
-console.log(basicOp('-', 15, 18), -3);
-console.log(basicOp('*', 5, 5), 25);
-console.log(basicOp('/', 49, 7), 7);
+console.log(numberToString(67), '67');
+
+// .toString() - convert number to string
+// .toString(2) - for making value binary 
