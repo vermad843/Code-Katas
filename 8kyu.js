337df3e174082b6fde351e8b51bafb232b5256b0
vermad843 <vishnuverma7524@gmail.com>
2019-10-29 15:23:02 +0530

    episode 27

--- 8kyu.js
+++ 8kyu.js
@@ -1,17 +1,20 @@
-function squareArea(A){//A is a length of a arc 
-//    circumference  of  a circle = 2 PI r 
-//    A = 2 PI r/4 (because we are talking about 1/4 part of a circle)
-//      2 PI r  = A * 4 //look at this  carefully 
-	const circumference = A * 4 ;
-	const radius = circumference/(2 * Math.PI);
-	const areaOfSquare = radius**2;  
-	return Math.floor(areaOfSquare*100)/100
-}
+// function squareArea(A){//A is a length of a arc 
+// 	//    circumference  of  a circle = 2 PI r 
+// 	//    A = 2 PI r/4 (because we are talking about 1/4 part of a circle)
+// 	//      2 PI r  = A * 4 //look at this  carefully 
+// 		const circumference = A * 4 ;
+// 		const radius = circumference/(2 * Math.PI);
+// 		const areaOfSquare = radius**2;  
+// 		return Math.floor(areaOfSquare*100)/100
+// 	}
+	
+	
+	
+	
+	
+// 	console.log(squareArea(2), 1.62);
+// 	console.log(squareArea(0), 0);
+// 	console.log(squareArea(14.05), 80);
 
 
 
-
-
-console.log(squareArea(2), 1.62);
-console.log(squareArea(0), 0);
-console.log(squareArea(14.05), 80);
---
+++ epi_27/katas.js
@@ -0,0 +1,25 @@
+function basicOp(operation, value1, value2)
+{
+  const operations = {
+      '+' : (a, b) => a + b,
+      '-' : (a, b) => a - b, 
+      '*' : (a, b) => a * b, 
+      '/' : (a, b) => a / b,
+  };
+
+  const operationFn = operations[operation];
+
+  return operationFn(value1, value2)
+}
+
+// create a object called operations 
+// define the keys and values there 
+// from operations use bracket notation to 
+// access operation 
+//then return the operationFn
+
+
+console.log(basicOp('+', 4, 7), 11);
+console.log(basicOp('-', 15, 18), -3);
+console.log(basicOp('*', 5, 5), 25);
+console.log(basicOp('/', 49, 7), 7);
