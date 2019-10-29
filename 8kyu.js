fa598402bc5df56f6a13c54f1768be414bb6b8d6
vermad843 <vishnuverma7524@gmail.com>
2019-10-29 17:20:30 +0530

    volume and area of a box

--- 8kyu.js
+++ 8kyu.js
@@ -17,16 +17,38 @@
 // 	console.log(squareArea(14.05), 80);
 
 
-function warnTheSheep(queue) {
-    const wolfAtIndex0 =  queue.reverse();
-    const wolfPosition =  wolfAtIndex0.indexOf('wolf')
-  return wolfPosition === 0 ? 'Pls go away and stop eating my sheep' : `Oi! Sheep number ${ wolfPosition}! You are about to be eaten by a wolf!`;
-  }
+// function warnTheSheep(queue) {
+//     const wolfAtIndex0 =  queue.reverse();
+//     const wolfPosition =  wolfAtIndex0.indexOf('wolf')
+//   return wolfPosition === 0 ? 'Pls go away and stop eating my sheep' : `Oi! Sheep number ${ wolfPosition}! You are about to be eaten by a wolf!`;
+//   }
 
 
-console.log(warnTheSheep(["sheep", "sheep", "sheep", "sheep", "sheep", "wolf", "sheep", "sheep"]),
- "Oi! Sheep number 2! You are about to be eaten by a wolf!");
-console.log(warnTheSheep(["sheep", "wolf", "sheep", "sheep", "sheep", "sheep", "sheep"]), "Oi! Sheep number 5! You are about to be eaten by a wolf!");
-console.log(warnTheSheep(["wolf", "sheep", "sheep", "sheep", "sheep", "sheep", "sheep"]), "Oi! Sheep number 6! You are about to be eaten by a wolf!");
-console.log(warnTheSheep(["sheep", "wolf", "sheep"]), "Oi! Sheep number 1! You are about to be eaten by a wolf!");
-console.log(warnTheSheep(["sheep", "sheep", "wolf"]), "Pls go away and stop eating my sheep");
+// console.log(warnTheSheep(["sheep", "sheep", "sheep", "sheep", "sheep", "wolf", "sheep", "sheep"]),
+//  "Oi! Sheep number 2! You are about to be eaten by a wolf!");
+// console.log(warnTheSheep(["sheep", "wolf", "sheep", "sheep", "sheep", "sheep", "sheep"]), "Oi! Sheep number 5! You are about to be eaten by a wolf!");
+// console.log(warnTheSheep(["wolf", "sheep", "sheep", "sheep", "sheep", "sheep", "sheep"]), "Oi! Sheep number 6! You are about to be eaten by a wolf!");
+// console.log(warnTheSheep(["sheep", "wolf", "sheep"]), "Oi! Sheep number 1! You are about to be eaten by a wolf!");
+// console.log(warnTheSheep(["sheep", "sheep", "wolf"]), "Pls go away and stop eating my sheep");
+
+
+
+
+// 
+// volume = w * h * d 
+// totalSurfaceArea =  2(h × W) + 2(h × L) + 2(W × L)
+//  length l = depth(d)
+
+function getSize(w, h, d){
+ const volume = w*h*d;
+ const area =   2*(w*h) + 2*(h*d)  + 2*(w*d);
+return [area,volume];
+}
+
+
+
+
+console.log(getSize(4, 2, 6), 48);
+console.log(getSize(10, 10, 10), [600, 1000]);
+console.log(getSize(4, 2, 6), 88);
+console.log(getSize(4, 2, 6), 48);
