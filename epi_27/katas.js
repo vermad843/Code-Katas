dabe753f9920cd406cb30ee372290dfa2f877b5c
vermad843 <vishnuverma7524@gmail.com>
2019-10-30 22:04:27 +0530

    {,[ ,) problem

--- epi_27/katas.js
+++ epi_27/katas.js
@@ -68,10 +68,52 @@
 
 
 
-function solution(str, ending){
-  return str.endsWith(ending);
+// function solution(str, ending){
+//   return str.endsWith(ending);
+// }
+
+
+// console.log(solution('abcde', 'cde'), true)
+// console.log(solution('abcde', 'abc'), false)
+
+
+
+// 
+
+function isOpeningBrace(brace) {
+  return  brace === '(' || brace === '[' || brace === '{';
+}
+function validBraces(braces){
+  const braceStack = [];
+  
+  for (let i = 0; i < braces.length; i++) {
+      const brace = braces[i];
+     if(isOpeningBrace(brace)) {
+        braceStack.push(brace);
+     }else {
+       const lastBrace = braceStack.pop();
+       if(!lastBrace) return false;
+       if(lastBrace === '(' && brace !== ')') {
+         return false;
+       }
+       if(lastBrace === '[' && brace !== ']') {
+        return false;
+      }
+      if(lastBrace === '{' && brace !== '}') {
+        return false;
+      }
+     }
+  }
+  return braceStack.length === 0;
 }
 
 
-console.log(solution('abcde', 'cde'), true)
-console.log(solution('abcde', 'abc'), false)
+
+console.log(validBraces( "()" ), true);
+console.log(validBraces( "[(])" ), false);
+console.log(validBraces("(){}[]"  ) ,  true);
+console.log(validBraces("([{}])"  ) ,  true);
+console.log(validBraces("(}"      ) ,  false);
+console.log(validBraces("[(])"    ) ,  false);
+console.log(validBraces("[({})](]") ,  false);
+console.log(validBraces("}}]]))}])"), false);
