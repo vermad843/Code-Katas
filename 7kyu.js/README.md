c0a466740f266e717032157ccc49733a4f22d9bf
vermad843 <vishnuverma7524@gmail.com>
2019-11-09 18:01:46 +0530

    growingPlant

--- 7kyu.js/README.md
+++ 7kyu.js/README.md
@@ -1,1 +1,5 @@
 [x] https://www.codewars.com/kata/sum-of-cubes/train/javascript
+
+
+
+[x]https://www.codewars.com/kata/simple-fun-number-74-growing-plant/train/javascript
--- 7kyu.js/kata.js
+++ 7kyu.js/kata.js
@@ -9,3 +9,22 @@ function sumCubes(n){
 
 [ [1, 1], [2, 9], [3, 36], [4, 100], [10, 3025], [123, 58155876] ].forEach(([n,exp]) => 
     console.log(sumCubes(n), exp))
+
+
+
+
+
+// 
+
+function growingPlant(upSpeed, downSpeed, desiredHeight) {  
+    let i = 1;
+    for (let height = upSpeed; height < desiredHeight; height = height + upSpeed){
+      height = height - downSpeed;
+       i++ ;
+    }
+    return i;
+  }
+
+
+console.log(growingPlant(100,10,910),10)
+console.log(growingPlant(10,9,4),1)
