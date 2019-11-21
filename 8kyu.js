e3e49bf10f18246b158f2f98d99e77b86473dc40
vermad843 <vishnuverma7524@gmail.com>
2019-11-21 10:31:09 +0530

    UFC

--- 8kyu.js
+++ 8kyu.js
@@ -1856,3 +1856,16 @@ function unusualFive() {
 
 
 console.log(unusualFive(), 5);
+
+
+
+// 
+
+var quote = function(fighter) {
+  return fighter.toLowerCase() === 'george saint pierre' ? "I am not impressed by your performance." : "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+};
+
+console.log(quote('george saint pierre'), "I am not impressed by your performance.")
+console.log(quote('conor mcgregor'), "I'd like to take this chance to apologize.. To absolutely NOBODY!")
+console.log(quote('George Saint Pierre'), "I am not impressed by your performance.")
+console.log(quote('Conor McGregor'), "I'd like to take this chance to apologize.. To absolutely NOBODY!")
