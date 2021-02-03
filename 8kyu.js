b91d59a78079aa588ad49585a7beedad9dbaddd0
vermad843 <vishnuverma7524@gmail.com>
2021-02-03 19:21:46 +0530

    if else if

--- 8kyu.js
+++ 8kyu.js
@@ -3670,3 +3670,15 @@ console.log(excludingVatPrice(123), 106.96);
 
 // 
 
+
+function saleHotdogs(n){
+   if(n < 5) return n*100;
+   if(n >= 5 && n<10) return n * 95;else return n * 90;
+ }
+
+console.log(saleHotdogs(1),100);
+console.log(saleHotdogs(4),400);
+console.log(saleHotdogs(5),475);
+console.log(saleHotdogs(9),855);
+console.log(saleHotdogs(10),900);
+console.log(saleHotdogs(100),9000);
